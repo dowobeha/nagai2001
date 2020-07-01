5085900ad13efd4d7cd2fd2dfe9fbdc17579d663
Lane Schwartz <dowobeha@gmail.com>
2020-07-01 14:53:02 -0500

    Removed stale commented-out code

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,43 +1,9 @@
 #!/usr/bin/env python3.9
 
 from collections import namedtuple
-from enum import Enum
 import re
 import sys
 from typing import List
-
-
-# class Gloss:
-#
-#     def __init__(self):
-#         self.orthographicWords: List[str] = list()
-#         self.phoneticWords:     List[str] = list()
-#         self.lexicalWords:      List[str] = list()
-#         self.glossedWords:      List[str] = list()
-#         self.freeTranslation: str = None
-#         self.documentID: str = None
-#         self.sentenceID: int = None
-#
-#     def __bool__(self) -> bool:
-#         if (len(self.orthographicWords) > 0 and
-#                 len(self.phoneticWords) > 0 and
-#                 len(self.glossedWords) > 0 and
-#                 self.freeTranslation is not None and
-#                 self.documentID is not None and
-#                 self.sentenceID is not None):
-#             return True
-#         else:
-#             return False
-#
-#     def __str__(self) -> str:
-#         s = f"{self.documentID}.{self.sentenceID}\t"
-#         s += "\t" + "\t".join(self.orthographicWords) + "\n"
-#         s += "\t" + "\t".join(self.phoneticWords) + "\n"
-#         if len(self.lexicalWords) > 0:
-#             s += "\t" + "\t".join(self.lexicalWords) + "\n"
-#         s += "\t" + "\t".join(self.glossedWords) + "\n"
-#         s += "\t" + self.freeTranslation
-#         return s
 
 
 class ExtractedGloss:
@@ -104,8 +70,11 @@ class ExtractedGloss:
             print(self.underlyingWords)
             sys.exit(-1)
 
+        for word in self.orthographicWords:
+            print(word)
+
     def __str__(self):
-        #lines = '\n'.join(self.lines)
+        # lines = '\n'.join(self.lines)
         return f"==============================\nSentence {self.document}.{self.sentence}\n"
 
 
@@ -135,88 +104,6 @@ def extractGlosses(input_file: str) -> [ExtractedGloss]:
                                           sentence=start_positions[i].sentence,
                                           lines=lines[start_positions[i].start:]))
     return glosses
-    #for position in glosses:
-    #    print(position)
-
-# class Status(Enum):
-#     Beginning = 0
-#     ReadOrthography = 1
-#     ReadPhonetic = 2
-#     ReadGlossedEnglish = 3
-#     ReadLexical = 4
-#     ReadBlank = 5
-#     ReadTranslation = 6
-#
-#
-# def extract(*, input_file: str, output_file: str) -> [Gloss]:
-#     glosses: [Gloss] = list()
-#     status = Status.Beginning
-#     gloss = Gloss()
-#     with open(input_file, 'rt') as f:
-#         for l in f:
-#             # print(f"line was =\"{l}\"")
-#             #if l.startswith("16."):
-#             #    x = 1
-#             line = l.strip()  # l.strip("\r\n\t ")
-#             # print(f"line  is =\"{line}\"")
-#             if status is Status.Beginning and ("\t" in line or "-" in line):
-#                 parts: List[str] = line.split()
-#                 if len(glosses) == 0:
-#                     #print(f"line=\"{line}\"")
-#                     documentID: str = parts.pop(0)
-#                     sentenceID: int = 0
-#                 else:
-#                     try:
-#                         sentenceID = int(parts[0].strip("."))
-#                         parts = parts[1:]
-#                     except ValueError:
-#                         pass
-#                 gloss.documentID = documentID.strip(".") # f"doc{documentID}x"
-#                 gloss.sentenceID = sentenceID
-#                 gloss.orthographicWords.extend(parts)
-#                 status = Status.ReadOrthography
-#             elif status is Status.ReadOrthography:
-#                 parts: List[str] = line.split()
-#                 gloss.phoneticWords.extend(parts)
-#                 status = Status.ReadPhonetic
-#             elif status is Status.ReadPhonetic:
-#                 parts: List[str] = line.split()
-#                 gloss.glossedWords.extend(parts)
-#                 status = Status.ReadGlossedEnglish
-#             elif status is Status.ReadGlossedEnglish:
-#                 if "<" in line:
-#                     parts: List[str] = line.split()
-#                     gloss.lexicalWords.extend(parts)
-#                     status = Status.ReadLexical
-#                 elif not line:
-#                     status = Status.Beginning
-#             #elif (status is Status.ReadGlossedEnglish and "<" not in line) or (status is Status.ReadLexical):
-#             #    if line:
-#             #        print(f"WARNING: Expected blank line but found \"{line}\"")
-#             #        sys.exit(-2)
-#             #    else:
-#             #        status = Status.Beginning
-#             elif not line:
-#                 status = Status.Beginning
-#             elif "\t" not in line and "-" not in line:
-#                 if line:
-#                     if gloss.freeTranslation:
-#                         print(f"WARNING:\tTranslation {gloss.documentID}.{gloss.sentenceID} is already set to " +
-#                               f"\"{gloss.freeTranslation}\"" +
-#                               f", but now setting to \"{line}\"")
-#                     gloss.freeTranslation = line
-#
-#             else:
-#                 if line:
-#                     print(f"WARNING:\t{status}\tReached unexpected state\t\"{line}\"")
-#
-#             if gloss:
-#                 print(f"{gloss}\n")
-#                 glosses.append(gloss)
-#                 gloss = Gloss()
-#                 status = Status.Beginning
-#
-#     return glosses
 
 
 if __name__ == "__main__":
@@ -225,4 +112,4 @@ if __name__ == "__main__":
         sys.exit(-1)
     else:
         result = extractGlosses(input_file=sys.argv[1])  # extract(input_file=sys.argv[1], output_file=sys.argv[2])
-        #print(len(result))
+        # print(len(result))
